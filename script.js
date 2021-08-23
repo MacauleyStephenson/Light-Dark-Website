8b4c08fbf490e2330ce6cdaef403656c3b665b38
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-08-23 09:38:28 +0100

    nullified bugs for the sun/moon icon, added a local storage feature to remember the input from the user

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function darkMode(){
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun', 'fa-moon');
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     imageMode('dark');
 }
 
@@ -28,7 +28,7 @@ function lightMode(){
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon', 'fa-sun');
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     imageMode('light');
 }
 
@@ -37,12 +37,25 @@ function lightMode(){
 function switchTheme(event){
     if(event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); 
         darkMode();
     }else{
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'dark');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch/addEventListener('change', switchTheme);
+
+//Check local storage for theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark'){
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
