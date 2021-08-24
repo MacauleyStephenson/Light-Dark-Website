c7c4c328b31420f5e09a4892b9d422dda0232583
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-08-24 13:17:27 +0100

    slight code refactoring, more dynamic variables added

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
+const images = document.getElementById('image1', 'image2', 'image3');
 const textBox = document.getElementById('text-box');
-
+let DARK_THEME = 'dark';
+let LIGHT_THEME = 'light';
 
 // Dark or light images
 function imageMode(color){
@@ -14,35 +13,27 @@ function imageMode(color){
     image3.src =`img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode Styles
-function darkMode(){
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
-    imageMode('dark');
+function toggleDarkLightMode(isDark){
+    nav.style.backgroundColor = isDark ?'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') :
+     toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    isDark ? imageMode(DARK_THEME) : imageMode(LIGHT_THEME);
 }
 
-// Light Mode Styles
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    imageMode('light');
-}
 
 
 // Switch Theme Dynamically
 function switchTheme(event){
     if(event.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); 
-        darkMode();
+        document.documentElement.setAttribute('data-theme', DARK_THEME);
+        localStorage.setItem('theme', DARK_THEME); 
+        toggleDarkLightMode(true);
     }else{
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'dark');
-        lightMode();
+        document.documentElement.setAttribute('data-theme', LIGHT_THEME);
+        localStorage.setItem('theme', LIGHT_THEME);
+        toggleDarkLightMode(false);
     }
 }
 
@@ -54,8 +45,8 @@ const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark'){
+    if (currentTheme === DARK_THEME){
         toggleSwitch.checked = true;
-        darkMode();
+        toggleDarkLightMode();
     }
 }
